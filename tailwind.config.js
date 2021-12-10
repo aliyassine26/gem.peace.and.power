fefc033b5921fb1766cc9d58323fbabb87d7e019
aliyassine26 <47665097+aliyassine26@users.noreply.github.com>
2021-12-10 17:51:17 +0200

    homepage v1

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+
+        'new_color1': '#461F3D',
+        'highlight_color1': '#F3CADF',
+        'button1': '#C78AA8',
+
+      },
+
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
