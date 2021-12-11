31535de6cf58d588024a6e4483fe153f59a5fb25
aliyassine26 <47665097+aliyassine26@users.noreply.github.com>
2021-12-11 22:12:45 +0200

    cart v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
         'new_color1': '#461F3D',
         'highlight_color1': '#F3CADF',
         'button1': '#C78AA8',
+        'button2': '#7A199C',
         'moon_date': '#FFBEBE',
+        'cart_bg': '#E8EEFF',
 
       },
       backgroundSize: {
@@ -18,8 +20,17 @@ module.exports = {
         '90%': '90%',
         '50%': '50%',
         '16': '4rem',
+      },
+      width: {
+        '22': '5.5rem',
+        '25': '6.25rem',
+        '30': '7.5rem',
+      },
+      height: {
+        '22': '5.5rem',
+        '25': '6.25rem',
+        '30': '7.5rem',
       }
-
 
     },
   },
